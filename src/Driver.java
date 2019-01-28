8c13c3902c15392eb66ab2d501f4b2810ce05125
Oliver <revi99@me.com>
2019-01-28 09:28:51 -0500

    Basic Driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Bye");
-        System.out.println("test");
 
-        System.out.println("hi me");
+        Scanner in = new Scanner(System.in);
 
-        System.out.println("hik");
+        System.out.println("1. Tic-Tac-Toe (Human vs Human)\n" +
+                "2. Tic-Tac-Toe (Human vs Beginner AI)\n" +
+                "3. Tic-Tac-Toe (Human vs Pro AI)\n" +
+                "4. Connect 4 (Human vs Human)\n" +
+                "5. Connect 4 (Human vs Beginner AI)\n" +
+                "6. Connect 4 (Human vs Pro AI)\n" +
+                "Select your game (1-6): ");
+        int game = in.nextInt();
 
-        System.out.println("hello");
+        System.out.println("You picked: " + game);
 
     }
 }
