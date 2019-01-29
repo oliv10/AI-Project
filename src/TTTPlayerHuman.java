8045eb108732ad3751154553668c74eba92a9930
Oliver <revi99@me.com>
2019-01-29 13:55:24 -0500

    Added Human Player

---
+++ src/TTTPlayerHuman.java
@@ -0,0 +1,11 @@
+public class TTTPlayerHuman extends Player {
+
+    public TTTPlayerHuman(String name){
+        super(name);
+    }
+
+    @Override
+    public String getMove(Board board) {
+        return null;
+    }
+}
