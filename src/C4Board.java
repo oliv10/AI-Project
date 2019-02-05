4e1983fd4f5248e44552a4ad3e47b470e69884dc
Oliver <revi99@me.com>
2019-02-05 10:42:51 -0500

    Connect 4 Board Class Created - Extended from Board
    Methods Written (Full Functional)
    -placePiece
    -retractPiece

---
+++ src/C4Board.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+public class C4Board extends Board {
+
+    public static final int ROWS = 6;
+    public static final int COLS = 7;
+
+    public C4Board(){
+        super(ROWS, COLS);
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                set(r, c, "-");
+            }
+        }
+    }
+
+    @Override
+    public void placePiece(String loc, String player) {
+        for (int x=ROWS-1; x >= 0; x--){
+            Comparable check = theBoard[x][Integer.parseInt(loc)-1];
+            if (check.equals("-")){
+                theBoard[x][Integer.parseInt(loc)-1] = player;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void retractPiece(String loc) {
+        for (int x=0; x<ROWS; x++){
+            Comparable check = theBoard[x][Integer.parseInt(loc)-1];
+            if (!check.equals("-")){
+                theBoard[x][Integer.parseInt(loc)-1] = "-";
+                break;
+            }
+        }
+    }
+
+    public ArrayList<String> getEmptyLocs() {
+        ArrayList<String> empty = new ArrayList<>();
+        for(int r = 0; r < theBoard.length; r++) {
+            for(int c = 0; c < theBoard[0].length; c++) {
+                if(!theBoard[r][c].equals("X") && !theBoard[r][c].equals("O"))
+                    empty.add((String) theBoard[r][c]);
+            }
+        }
+        return empty;
+    }
+
+    @Override
+    public boolean isWinner(String player) {
+        return false;
+    }
+
+    @Override
+    public boolean isGameOver() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "1234567\n" + super.toString();
+    }
+}
