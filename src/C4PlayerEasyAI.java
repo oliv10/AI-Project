ce4ae611184cd56bc55f7fee6eb1f1e087036f2a
Oliver <revi99@me.com>
2019-02-12 11:20:30 -0500

    C4PlayerEasyAI Created and Fully Functional

---
+++ src/C4PlayerEasyAI.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class C4PlayerEasyAI extends Player {
+
+    private Random r = new Random();
+    private String otherPlayer = "";
+
+    public C4PlayerEasyAI(String name){
+        super(name);
+        if (name.equals("R")){
+            this.otherPlayer = "Y";
+        }
+        else{
+            this.otherPlayer = "R";
+        }
+    }
+
+    public String getMove(Board board){
+        for (int loc = 1; loc <= 7; loc++){
+            String move = Integer.toString(loc);
+            board.placePiece(move, name);
+            if (board.isWinner(name)){
+                board.retractPiece(move);
+                System.out.println("AI " + name + " Moved to: " + move);
+                return move;
+            }
+            else if (!board.isWinner(name)){
+                board.retractPiece(move);
+                board.placePiece(move, otherPlayer);
+                if (board.isWinner(otherPlayer)){
+                    board.retractPiece(move);
+                    System.out.println("AI " + name + " Moved to: " + move);
+                    return move;
+                }
+                else{
+                    board.retractPiece(move);
+                }
+            }
+        }
+        String move = Integer.toString(r.nextInt(7));
+        System.out.println("AI " + name + " Moved to: " + move);
+        return move;
+    }
+
+}
